828c8f1d384b64d24683ff5f9c8e14ed7c316864
Ghaith <Gaalmatboona@gmail.com>
2026-09-24 12:37:13 +0400

    Mobile-More-Tab-Color

--- src/components/BusinessHub.js
+++ src/components/BusinessHub.js
@@ -4,13 +4,15 @@ import Sidebar from "./Sidebar";
 import PageHeader from "./PageHeader";
 import PostServiceTab from "./PostServiceTab";
 import PostRFQTab from "./PostRFQTab";
+import MobileBottomNav from "./MobileBottomNav";
 import "./BusinessHub.css";
 
 function BusinessHub() {
   const [activeTab, setActiveTab] = useState("service");
+  const [activeNav, setActiveNav] = useState("marketplace");
 
   return (
-    <div className="bh-app">
+    <div className="bh-app bh-app--mnav">
       <TopNavbar />
       <div className="bh-body">
         <Sidebar />
@@ -20,8 +22,11 @@ function BusinessHub() {
           {activeTab === "service" ? <PostServiceTab /> : <PostRFQTab />}
         </main>
       </div>
+
+      {/* Phones only (<= 768px): bottom tab bar + "More" sheet */}
+      <MobileBottomNav active={activeNav} onNavigate={setActiveNav} />
     </div>
   );
 }
-
+      
 export default BusinessHub;
